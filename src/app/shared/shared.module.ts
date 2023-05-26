57b223ac5d160c6bdce06dcf202b25391706c22e
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-05-26 16:53:41 -0500

    updated form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-// import { ModalService } from '../services/modal.service';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
 
 
 
@@ -11,18 +13,22 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
-  // providers: [
-  //   ModalService
-  // ]
+  providers: [
+    provideEnvironmentNgxMask()
+  ]
 })
 export class SharedModule { }
