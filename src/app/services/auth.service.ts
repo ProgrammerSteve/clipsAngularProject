341ce60d49c7da2db50843b8a2730baa6b463181
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-08-23 11:58:10 -0500

    updated nav component to subscribe to auth observable for login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs'
 import IUser from '../models/user.model';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,13 @@ import IUser from '../models/user.model';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
 
+  public isAuthenticated$: Observable<boolean>
+
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users')
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
   }
 
 
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,32 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode, isDevMode } from '@angular/core';
+
+import { environment } from './environments/environment';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+if (!isDevMode()) {
+  enableProdMode()
+}
+
+
+firebase.initializeApp(environment.firebase)
+
+let appInit = false
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!appInit) {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+    appInit = true
+  }
+
+})
+
+
+
