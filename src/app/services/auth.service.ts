8176e16d6c25a1bfd82c8d927fa505da072177db
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-08-29 21:15:20 -0500

    finished off the login modal

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs'
 import IUser from '../models/user.model';
-import { map } from 'rxjs/operators'
+import { map, delay } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,16 @@ export class AuthService {
 
   public isAuthenticated$: Observable<boolean>
 
+  public isAuthenticatedWithDelay$: Observable<boolean>
+
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
+    )
+
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+      delay(1000)
     )
   }
 
