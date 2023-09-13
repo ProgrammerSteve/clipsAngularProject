5a1e9c49d7647451084bd328952380e958bf27b6
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-09-13 17:23:05 -0500

    added drag events and custom directive for file uploading

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  isDragover = false
+  file: File | null = null
+  nextStep = false
+
+  constructor() {
+
+  }
+  storeFile($event: Event) {
+    this.isDragover = false
+
+    console.log($event)
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+    this.nextStep = true
+  }
+
 
 }
